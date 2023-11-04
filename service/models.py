e10c9d70d304640473c3ba437c8d88f0dc02a131
Jack Pang <45305339+Jackpanger@users.noreply.github.com>
2023-11-04 17:04:24 -0400

    Update models.py
    Make condition parameter is always uppercase

--- service/models.py
+++ service/models.py
@@ -174,6 +174,6 @@ class Inventory(db.Model):
             condition = Condition(int(condition))
             logger.info("Processing condition query for %s ...", condition)
         else:
-            condition = getattr(Condition, condition)
+            condition = getattr(Condition, str.upper(condition))
             logger.info("Processing condition query for %s ...", condition)
         return cls.query.filter(cls.condition == condition)

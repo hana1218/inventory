d42049867a45e7ede9c2eac1f699e0903310c208
Jessie Yu <jingxi_yu@10-16-155-145.dynapool.wireless.nyu.edu>
2023-11-04 18:33:15 -0400

    restock item to level

--- service/models.py
+++ service/models.py
@@ -174,7 +174,7 @@ class Inventory(db.Model):
             condition = Condition(int(condition))
             logger.info("Processing condition query for %s ...", condition)
         else:
-            condition = condition.replace("-", "_")
+            # condition = condition.replace("-", "_")
             condition = getattr(Condition, str.upper(condition))
             logger.info("Processing condition query for %s ...", condition)
         return cls.query.filter(cls.condition == condition)
